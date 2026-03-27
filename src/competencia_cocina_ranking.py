3f234de734dd115ff57e47b6e09a28a2f43c53fa
alex ceballos <alexleonel2006@gmail.com>
2026-03-27 16:03:00 -0300

    agregue el ejercicio de competencia de cocina ranking

--- src/competencia_cocina_ranking.py
+++ src/competencia_cocina_ranking.py
@@ -49,3 +49,65 @@ rounds = [
             }
     }
 ]
+
+
+def imprimirTablaRonda(ordenado):
+    for participante,datos in ordenado:
+        print(f'{participante:10} {datos["puntaje"]:8} {datos["rondas ganadas"]:13} {datos["mejor ronda"]:18} {datos["promedio"]:16}')
+    print("")
+
+
+def mejorPuntajeRonda(puntaje,participante,mejorPuntaje,mejorCocinero):
+    if (mejorCocinero == "") and (mejorPuntaje == -1):
+        mejorCocinero = participante
+        mejorPuntaje = puntaje
+    else:
+        if (puntaje > mejorPuntaje):
+            mejorCocinero = participante
+            mejorPuntaje = puntaje
+    return mejorPuntaje,mejorCocinero
+
+def imprimirTablaFinal(ordenado):
+    print("Tabla de posiciones final:")
+    for participante,datos in ordenado:
+            print(f'{participante:10} {datos["puntaje"]:8} {datos["rondas ganadas"]:13} {datos["mejor ronda"]:18} {datos["promedio"]:16}')
+
+        
+def competenciaCocinaRanking():
+    tablaPosicionesFinal = {}
+    for i,ronda in enumerate(rounds):
+        posicion = i + 1
+        print(f'Ronda {posicion} - {ronda["theme"]}')
+        
+        participantes = ronda["scores"] #dicc de participantes
+        mejorCocinero = ""
+        mejorPuntaje = -1
+        for participante in participantes: #participante es la clave(string) de participantes
+            puntaje = sum(participantes[participante].values())
+            
+            if participante not in tablaPosicionesFinal:
+                tablaPosicionesFinal[participante] = {
+                    "puntaje":puntaje,
+                    "rondas ganadas":0,
+                    "mejor ronda":puntaje,
+                    "promedio":puntaje
+                }
+            else:
+                tablaPosicionesFinal[participante]["puntaje"] += puntaje
+                tablaPosicionesFinal[participante]["promedio"] = tablaPosicionesFinal[participante]["puntaje"] / posicion
+                
+            if (puntaje > tablaPosicionesFinal[participante]["mejor ronda"]):
+                tablaPosicionesFinal[participante]["mejor ronda"] = puntaje
+            
+            mejorPuntaje,mejorCocinero = mejorPuntajeRonda(puntaje,participante,mejorPuntaje,mejorCocinero)
+        
+        tablaPosicionesFinal[mejorCocinero]["rondas ganadas"] += 1
+        print(f"Ganador: {mejorCocinero} ({mejorPuntaje})")
+            
+        ordenado = sorted(tablaPosicionesFinal.items(), key=lambda x:x[1]["puntaje"], reverse=True)
+        print("Cocinero      puntaje     rondas ganadas     mejor ronda      promedio")
+        
+        imprimirTablaRonda(ordenado)
+    imprimirTablaFinal(ordenado)
+    
+competenciaCocinaRanking()
